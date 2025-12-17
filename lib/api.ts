2e58be8da59adc89134c6ebfe92c7605b5350a04
Nayan Kar <167174966+NayanKarIITP@users.noreply.github.com>
2025-12-17 19:13:17 +0530

    Update API_URL for production and development

--- lib/api.ts
+++ lib/api.ts
@@ -1,6 +1,9 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:5000/api";
+const API_URL =
+  window.location.hostname === "localhost"
+    ? "http://localhost:5000/api"
+    : "https://carriercopilot-nk.onrender.com/api";
 
 const api = axios.create({
   baseURL: API_URL,
