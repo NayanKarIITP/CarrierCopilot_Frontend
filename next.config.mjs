ae5ba6737fe5b571f329959d881969d9c59fc2b8
Nayan Kar <167174966+nkar90@users.noreply.github.com>
2025-12-22 23:27:46 +0530

    Fix Vercel build: remove standalone output

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,22 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   output: "standalone",
+
+//   typescript: {
+//     ignoreBuildErrors: true,
+//   },
+
+//   images: {
+//     unoptimized: true,
+//   },
+// };
+
+// export default nextConfig;
+
+
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "standalone",
-
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-
   images: {
     unoptimized: true,
   },
