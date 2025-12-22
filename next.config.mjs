f33c4fa0ab2d5af8c16d5177df70e8c133bd7cef
Nayan Kar <167174966+nkar90@users.noreply.github.com>
2025-12-23 00:07:29 +0530

    message

--- next.config.mjs
+++ next.config.mjs
@@ -17,6 +17,12 @@
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  reactStrictMode: true,
+
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+
   images: {
     unoptimized: true,
   },
